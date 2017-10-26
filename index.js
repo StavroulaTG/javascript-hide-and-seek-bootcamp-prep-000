26f5de7fcd0ad9ace75c5d8dc6236eca86f36c5e
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-26 22:43:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('.ranked-list')
+    for (let i = 0; i < list.length; i++) {
+      list[i].innerHTML = (parseInt(list[i].innerHTML) + n)
+    }
+    return list;
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
